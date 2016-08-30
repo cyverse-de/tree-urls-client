0df6f7150e28cf70912ca1e26f6ea6ed0e646ca8
Ian McEwen <mian@cyverse.org>
2016-08-30 14:31:43 -0700

    Version 2.8.0 (+switch to org.cyverse)

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject org.iplantc/tree-urls-client "5.2.8.0"
+(defproject org.cyverse/tree-urls-client "2.8.0"
   :description "Client for the tree-urls service"
-  :url "https://github.com/cyverse/DE"
+  :url "https://github.com/cyverse-de/tree-urls-client"
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.iplantc/clojure-commons "5.2.8.0" :exclusions [buddy/buddy-sign metosin/compojure-api metosin/ring-http-response ring]]
+                 [org.cyverse/clojure-commons "2.8.0" :exclusions [buddy/buddy-sign metosin/compojure-api metosin/ring-http-response ring]]
                  [slingshot "0.12.2"]
                  [clj-http "2.0.0"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
