5ee4ff011a5b6a923b8105ec84f066bbd817505b
Ian McEwen <mian@cyverse.org>
2016-12-05 15:04:14 -0700

    Version 2.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/tree-urls-client "2.8.1"
+(defproject org.cyverse/tree-urls-client "2.8.2-SNAPSHOT"
   :description "Client for the tree-urls service"
   :url "https://github.com/cyverse-de/tree-urls-client"
   :license {:name "BSD Standard License"
